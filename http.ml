88031308645402590e8481c883451a9ac736a6b8
Martin Desharnais <martin.desharnais@gmail.com>
2015-07-12 20:29:42 -0400

    Draft implementation of file writing

--- http.ml
+++ http.ml
@@ -4,6 +4,10 @@ open Cohttp_lwt_unix
 
 let (<*>) f g x = f (g x)
 
+let get_filename req =
+  let path = Uri.path (Request.uri req) in
+  Sys.getcwd () ^ path
+
 let unimplemented body = Server.respond_string ~status:`OK ~body ()
 
 let forward_to_others ips req body =
@@ -24,23 +28,32 @@ let forward_to_others ips req body =
   )
 
 let get ips req body =
-  let path = Uri.path (Request.uri req) in
-  let fname = Sys.getcwd () ^ path in
+  let fname = get_filename req in
   Server.respond_file ~fname ()
-  >>= (function (r, body) as resp ->
+  >>= (function (r, _) as resp ->
     match Response.status r with
     | `Not_found -> forward_to_others ips req body
     | _ -> Lwt.return resp)
 
 let post = unimplemented "POST\n"
 
-let put = unimplemented "PUT\n"
+let put req body =
+  let filename = get_filename req in
+  let flags = [Unix.O_WRONLY; Unix.O_TRUNC] in
+  let mode = Lwt_io.output in
+  Lwt.catch
+    (function () -> Lwt_io.with_file ~flags ~mode filename
+      (Fn.flip Cohttp_lwt_body.write_body body <*> Lwt_io.write)
+      >>= function () -> Server.respond_string ~status:`OK ~body:"" ())
+    (function Unix.Unix_error (ENOENT, _, _) ->
+      unimplemented "PUT Forward\n"
+    | _ -> unimplemented "Unknown exception\n")
 
-let callback ips (ch, _) req body =
+let callback ips _ req body =
   match Request.meth req with
   | `GET -> get ips req body
   | `POST -> post
-  | `PUT -> put
+  | `PUT -> put req body
   | _ -> unimplemented "oops\n"
 
 let make_server ips () =
