a1892ef1c91811a1e4b518b180b9c081cba3f90e
SGrondin <github@simongrondin.name>
2015-07-16 09:59:53 -0400

    Merged and fixed

--- http.ml
+++ http.ml
@@ -11,21 +11,28 @@ let get_filename req =
 let unimplemented body = Server.respond_string ~status:`OK ~body ()
 
 let forward_to_others ips req body =
-  List.map ~f:(fun ip ->
-    let headers = Cohttp.Header.init_with "forwarded" "true" in
-    Client.get ~headers (Uri.with_host (Request.uri req) (Some ip))
-  ) ips
-  |> Lwt_list.filter_p (fun task ->
-    task >|= fun (res, _) -> Response.status res <> `Not_found
-  )
-  >>= (function
-  | [] -> Server.respond_string ~status:`Not_found ~body:"" ()
-  | x :: [] -> x
-  | xs ->
-    Lwt_list.map_p (map (Sexp.to_string <*> Response.sexp_of_t <*> fst)) xs
-    >>= fun ls ->
-      Server.respond_string ~status:`Internal_server_error ~body:(List.to_string ~f:Fn.id ls) ()
-  )
+  match Cohttp.Header.get (Request.headers req) "forwarded" with
+  | Some _ -> Server.respond_string ~status:`Not_found ~body:"" ()
+  | None ->
+    List.map ~f:(fun ip ->
+      let headers = Cohttp.Header.init_with "forwarded" "true" in
+      let uri = Request.uri req
+      |> fun u -> Uri.with_scheme u (Some "http")
+      |> fun u -> Uri.with_host u (Some ip)
+      in
+      Client.get ~headers uri
+    ) ips
+    |> Lwt_list.filter_p (fun task ->
+      task >|= fun (res, _) -> Response.status res <> `Not_found
+    )
+    >>= (function
+    | [] -> Server.respond_string ~status:`Not_found ~body:"" ()
+    | x :: [] -> x
+    | xs ->
+      Lwt_list.map_p (map (Sexp.to_string <*> Response.sexp_of_t <*> fst)) xs
+      >>= fun ls ->
+        Server.respond_string ~status:`Internal_server_error ~body:(List.to_string ~f:Fn.id ls) ()
+    )
 
 let get ips req body =
   let fname = get_filename req in
@@ -41,20 +48,27 @@ let put req body =
   let filename = get_filename req in
   let flags = [Unix.O_WRONLY; Unix.O_TRUNC] in
   let mode = Lwt_io.output in
-  Lwt.catch
-    (function () -> Lwt_io.with_file ~flags ~mode filename
-      (Fn.flip Cohttp_lwt_body.write_body body <*> Lwt_io.write)
-      >>= function () -> Server.respond_string ~status:`OK ~body:"" ())
-    (function Unix.Unix_error (ENOENT, _, _) ->
-      unimplemented "PUT Forward\n"
-    | _ -> unimplemented "Unknown exception\n")
+  try_lwt (
+    Lwt_io.with_file ~flags ~mode filename (Fn.flip Cohttp_lwt_body.write_body body <*> Lwt_io.write)
+    >>= function () ->
+        Server.respond_string ~status:`OK ~body:"" ()
+  ) with
+  | Unix.Unix_error (Unix.ENOENT, _, _) ->
+    unimplemented "PUT Forward\n"
+  | e ->
+    unimplemented (Exn.to_string_mach e)
 
 let callback ips _ req body =
-  match Request.meth req with
-  | `GET -> get ips req body
-  | `POST -> post
-  | `PUT -> put req body
-  | _ -> unimplemented "oops\n"
+  try_lwt (
+    match Request.meth req with
+    | `GET -> get ips req body
+    | `POST -> post
+    | `PUT -> put req body
+    | _ -> unimplemented "oops\n"
+  ) with
+  | e ->
+    print_endline (Exn.to_string_mach e);
+    unimplemented ("An exception occured: \n" ^ (Exn.to_string_mach e))
 
 let make_server ips () =
   let ctx = Cohttp_lwt_unix_net.init () in
