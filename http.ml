f9a95f0c6ffea4abe04c1dca9e3b3eaf5a17758d
SGrondin <github@simongrondin.name>
2015-07-16 11:56:24 -0400

    Implemented distributed lock for POST

--- http.ml
+++ http.ml
@@ -4,11 +4,18 @@ open Cohttp_lwt_unix
 
 let (<*>) f g x = f (g x)
 
+let locked = Hashtbl.create ~hashable:String.hashable ()
+
 let get_filename req =
   let path = Uri.path (Request.uri req) in
   Sys.getcwd () ^ path
 
 let unimplemented body = Server.respond_string ~status:`OK ~body ()
+
+let critical_error e =
+  let body = Exn.to_string_mach e in
+  print_endline body;
+  Server.respond_string ~status:`Internal_server_error ~body ()
 
 let forward_to_others ips meth req body =
   match Cohttp.Header.get (Request.headers req) "forwarded" with
@@ -26,7 +33,7 @@ let forward_to_others ips meth req body =
       task >|= fun (res, _) -> Response.status res = `OK
     )
     >>= (function
-    | [] -> Server.respond_string ~status:`Bad_request ~body:"" ()
+    | [] -> Server.respond_string ~status:`Not_found ~body:"" ()
     | x :: [] -> x
     | xs ->
       Lwt_list.map_p (map (Sexp.to_string <*> Response.sexp_of_t <*> fst)) xs
@@ -59,7 +66,38 @@ let get ips req body =
     | `Not_found -> forward_to_others ips `GET req body
     | _ -> return resp)
 
-let post = unimplemented "POST\n"
+let lock ips req body =
+  let fname = get_filename req in
+  try_lwt (
+    Lwt_io.with_file ~flags:[Unix.O_RDONLY] ~mode:Lwt_io.input fname (fun _ ->
+      Server.respond_string ~status:`Conflict ~body:"" ()
+    )
+  ) with
+  | Unix.Unix_error (Unix.ENOENT, _, _) ->
+    (* The file doesn't exist, lock it and return OK *)
+    Hashtbl.set locked fname true;
+    Server.respond_string ~status:`OK ~body:"" ()
+  | e -> critical_error e
+
+let post ips req body =
+  let fname = get_filename req in
+  match Hashtbl.find locked fname with
+  | Some el -> Server.respond_string ~status:`Conflict ~body:"This file already exists" ()
+  | None ->
+    forward_to_others ips (`Other "LOCK") (Request.make ~meth:(`Other "LOCK") (Request.uri req)) body
+    >>= fun (response, res_body) ->
+      match Response.status response with
+      | `OK | `Not_found ->
+        (try_lwt (
+          Lwt_io.with_file ~flags:[Unix.O_WRONLY; Unix.O_CREAT] ~mode:Lwt_io.output fname (fun ch ->
+            Lwt_io.write ch ""
+            >>= fun () ->
+            return (response, res_body)
+          )
+        ) with
+        | e -> critical_error e)
+      | `Conflict -> return (response, res_body)
+      | _ -> Server.respond_string ~status:`Internal_server_error ~body:(Response.sexp_of_t response |> Sexp.to_string) ()
 
 let put req body =
   let filename = get_filename req in
@@ -72,20 +110,22 @@ let put req body =
   ) with
   | Unix.Unix_error (Unix.ENOENT, _, _) ->
     unimplemented "PUT Forward\n"
-  | e ->
-    unimplemented (Exn.to_string_mach e)
+  | e -> critical_error e
+
+let delete ips req body =
+  unimplemented "DELETE"
 
 let callback ips _ req body =
   try_lwt (
     match Request.meth req with
     | `GET -> get ips req body
-    | `POST -> post
+    | `Other "LOCK" -> lock ips req body
+    | `POST -> post ips req body
     | `PUT -> put req body
+    | `DELETE -> delete ips req body
     | _ -> unimplemented "oops\n"
   ) with
-  | e ->
-    print_endline (Exn.to_string_mach e);
-    unimplemented ("An exception occured: \n" ^ (Exn.to_string_mach e))
+  | e -> critical_error e
 
 let make_server ips () =
   let ctx = Cohttp_lwt_unix_net.init () in
