4bb463f6f9a55c00678d784d08939202a69bde14
SGrondin <github@simongrondin.name>
2015-07-17 11:08:30 -0400

    HTTP status codes cleanup

--- http.ml
+++ http.ml
@@ -12,12 +12,14 @@ let get_filename req =
   let path = Uri.path (Request.uri req) in
   Sys.getcwd () ^ path
 
-let unimplemented body = Server.respond_string ~status:`OK ~body ()
+let unimplemented body = Server.respond_string ~status:`Method_not_allowed ~body ()
 
 let critical_error e =
   let body = Exn.to_string_mach e in
   print_endline body;
   Server.respond_string ~status:`Internal_server_error ~body ()
+
+let conflict () = Server.respond_string ~status:`Conflict ~body:"" ()
 
 let forward_to_others ips meth req body =
   match Cohttp.Header.get (Request.headers req) "forwarded" with
@@ -39,8 +41,7 @@ let forward_to_others ips meth req body =
     | x :: [] -> x
     | xs ->
       Lwt_list.map_p (map (Sexp.to_string <*> Response.sexp_of_t <*> fst)) xs
-      >>= fun ls ->
-        Server.respond_string ~status:`Internal_server_error ~body:(List.to_string ~f:Fn.id ls) ()
+      >>= fun ls -> critical_error (Failure (List.to_string ~f:Fn.id ls))
     )
 
 let add_trailing_slash_if_directory root filename =
@@ -72,9 +73,7 @@ let get ips req body =
 let lock ips req body =
   let fname = get_filename req in
   try_lwt (
-    Lwt_io.with_file ~flags:[Unix.O_RDONLY] ~mode:Lwt_io.input fname (fun _ ->
-      Server.respond_string ~status:`Conflict ~body:"" ()
-    )
+    Lwt_io.with_file ~flags:[Unix.O_RDONLY] ~mode:Lwt_io.input fname (fun _ -> conflict ())
   ) with
   | Unix.Unix_error (Unix.ENOENT, _, _) ->
     (* The file doesn't exist, lock it and return OK *)
@@ -86,7 +85,7 @@ let lock ips req body =
 let post ips req body =
   let fname = get_filename req in
   match Hashtbl.find locked fname with
-  | Some el -> Server.respond_string ~status:`Conflict ~body:"" ()
+  | Some el -> conflict ()
   | None ->
     forward_to_others ips (`Other "LOCK") (Request.make ~meth:(`Other "LOCK") (Request.uri req)) body
     >>= fun (response, res_body) ->
@@ -100,8 +99,8 @@ let post ips req body =
         ) with
         | e -> critical_error e)
       (* Not_found is returned by forward_to_others when no hosts said OK *)
-      | `Conflict | `Not_found -> Server.respond_string ~status:`Conflict ~body:"" ()
-      | _ -> Server.respond_string ~status:`Internal_server_error ~body:(Response.sexp_of_t response |> Sexp.to_string) ()
+      | `Conflict | `Not_found -> conflict ()
+      | _ -> critical_error (Failure (Response.sexp_of_t response |> Sexp.to_string))
 
 let put req body =
   let filename = get_filename req in
